82e7615179a7df03803c6c130e4f875c9cb0e645
Arne <arne.sailer@gmx.net>
2022-04-08 11:33:55 +0200

    make lists of dataframes

--- main.py
+++ main.py
@@ -13,7 +13,67 @@ if __name__ == '__main__':
         patients.append(sqldf("SELECT * FROM data "
                "WHERE id IS '" + id +"'"))
     #print(patients)
-    p0 = patients[0]
+
+    moods = []
+    circumplex_arousals = []
+    circumplex_valences = []
+    activitys = []
+    screens = []
+    calls = []
+    smss = []
+    builtins = []
+    communications = []
+    entertainments = []
+    finances = []
+    games = []
+    offices = []
+    others = []
+    socials = []
+    travels = []
+    unknowns = []
+    utilitiess = []
+    weathers = []
+
+    for patient in patients:
+        moods.append(sqldf("SELECT variable as mood, value, time FROM patient "
+          "WHERE variable IS 'mood'"))
+        circumplex_arousals.append(sqldf("SELECT variable as 'circumplex.arousal', value, time FROM patient "
+          "WHERE variable IS 'circumplex.arousal'"))
+        circumplex_valences.append(sqldf("SELECT variable as 'circumplex.valence', value, time FROM patient "
+          "WHERE variable IS 'circumplex.valence'"))
+        activitys.append(sqldf("SELECT variable as activity, value, time FROM patient "
+          "WHERE variable IS 'activity'"))
+        screens.append(sqldf("SELECT variable as screen, value, time FROM patient "
+          "WHERE variable IS 'screen'"))
+        calls.append(sqldf("SELECT variable as call, value, time FROM patient "
+          "WHERE variable IS 'call'"))
+        smss.append(sqldf("SELECT variable as sms, value, time FROM patient "
+          "WHERE variable IS 'sms'"))
+        builtins.append(sqldf("SELECT variable as 'appCat.builtin', value, time FROM patient "
+            "WHERE variable IS 'appCat.builtin'"))
+        communications.append(sqldf("SELECT variable as 'appCat.communication', value, time FROM patient "
+          "WHERE variable IS 'appCat.communication'"))
+        entertainments.append(sqldf("SELECT variable as 'appCat.entertainment', value, time FROM patient "
+          "WHERE variable IS 'appCat.entertainment'"))
+        finances.append(sqldf("SELECT variable as 'appCat.finance', value, time FROM patient "
+          "WHERE variable IS 'appCat.finance'"))
+        games.append(sqldf("SELECT variable as 'appCat.game', value, time FROM patient "
+          "WHERE variable IS 'appCat.game'"))
+        offices.append(sqldf("SELECT variable as 'appCat.office', value, time FROM patient "
+          "WHERE variable IS 'appCat.office'"))
+        others.append(sqldf("SELECT variable as 'appCat.other', value, time FROM patient "
+          "WHERE variable IS 'appCat.other'"))
+        socials.append(sqldf("SELECT variable as 'appCat.social', value, time FROM patient "
+          "WHERE variable IS 'appCat.social'"))
+        travels.append(sqldf("SELECT variable as 'appCat.travel', value, time FROM patient "
+          "WHERE variable IS 'appCat.travel'"))
+        unknowns.append(sqldf("SELECT variable as 'appCat.unknown', value, time FROM patient "
+          "WHERE variable IS 'appCat.unknown'"))
+        utilitiess.append(sqldf("SELECT variable as 'appCat.utilities', value, time FROM patient "
+          "WHERE variable IS 'appCat.utilities'"))
+        weathers.append(sqldf("SELECT variable as 'appCat.weather', value, time FROM patient "
+          "WHERE variable IS 'appCat.weather'"))
+
     p0_mood = sqldf("SELECT variable as mood, value, time FROM p0 "
           "WHERE variable IS 'mood'")
     p0_activity = sqldf("SELECT variable as activity, value, time FROM p0 "
